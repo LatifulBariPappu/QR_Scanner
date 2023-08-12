9edcb4a2195c14f481fd209c47d910d3a7324ced
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-08-12 13:54:08 +0600

    homeActivity

---
+++ app/src/main/java/com/example/qrscannerandgenerator/GenerateQRActivity.java
@@ -0,0 +1,14 @@
+package com.example.qrscannerandgenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GenerateQRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_qractivity);
+    }
+}
--- app/src/main/java/com/example/qrscannerandgenerator/HomeActivity.java
+++ app/src/main/java/com/example/qrscannerandgenerator/HomeActivity.java
@@ -2,15 +2,37 @@ package com.example.qrscannerandgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button generateQRBtn,scanQRBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        generateQRBtn=findViewById(R.id.idBtnGenerateQR);
+        scanQRBtn=findViewById(R.id.idBtnScanQR);
+
+        generateQRBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(HomeActivity.this,GenerateQRActivity.class);
+                startActivity(i);
+
+            }
+        });
+        scanQRBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(HomeActivity.this,ScanQRActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
