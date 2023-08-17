9e4df679f8751e17442ca290a124a3b40b26c9a6
Latiful Bari Pappu <126404684+LatifulBariPappu@users.noreply.github.com>
2023-08-18 01:15:26 +0600

    run successfully

--- app/src/main/java/com/example/qrscannerandgenerator/ScanQRActivity.java
+++ app/src/main/java/com/example/qrscannerandgenerator/ScanQRActivity.java
@@ -1,16 +1,17 @@
 package com.example.qrscannerandgenerator;
 
-import androidx.annotation.RequiresApi;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import eu.livotov.labs.android.camview.ScannerLiveView;
+import eu.livotov.labs.android.camview.scanner.decoder.zxing.ZXDecoder;
 
 public class ScanQRActivity extends AppCompatActivity {
 
@@ -23,8 +24,36 @@ public class ScanQRActivity extends AppCompatActivity {
 
         scannerLiveView=findViewById(R.id.camView);
         scannerTv=findViewById(R.id.idTVSannedData);
-    }
+        if(checkPermission()){
+            Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
+        }else{
+            requestPermission();
+        }
 
+        scannerLiveView.setScannerViewEventListener(new ScannerLiveView.ScannerViewEventListener() {
+            @Override
+            public void onScannerStarted(ScannerLiveView scanner) {
+                Toast.makeText(ScanQRActivity.this, "Scanner Started..", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onScannerStopped(ScannerLiveView scanner) {
+                Toast.makeText(ScanQRActivity.this, "Scanner Stopped..", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onScannerError(Throwable err) {
+
+            }
+
+            @Override
+            public void onCodeScanned(String data) {
+                scannerTv.setText(data);
+
+            }
+        });
+    }
     private boolean checkPermission(){
         int camera_permission= ContextCompat.checkSelfPermission(getApplicationContext(),CAMERA_SERVICE);
         int vibrate_permission=ContextCompat.checkSelfPermission(getApplicationContext(),VIBRATOR_SERVICE);
@@ -34,5 +63,33 @@ public class ScanQRActivity extends AppCompatActivity {
     private void requestPermission(){
         int permissionCode=200;
         ActivityCompat.requestPermissions(this,new String[]{CAMERA_SERVICE,VIBRATOR_SERVICE},permissionCode);
+    }
+
+    @Override
+    protected void onPause() {
+        scannerLiveView.stopScanner();
+        super.onPause();
+    }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        ZXDecoder decoder=new ZXDecoder();
+        decoder.setScanAreaPercent(0.8);
+        scannerLiveView.setDecoder(decoder);
+        scannerLiveView.startScanner();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length>0){
+            boolean cameraAccepted=grantResults[0]==PackageManager.PERMISSION_GRANTED;
+            boolean vibrateionAccepted=grantResults[1]==PackageManager.PERMISSION_GRANTED;
+            if(cameraAccepted && vibrateionAccepted){
+                Toast.makeText(this, "Permission Granted...", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "Permission Denied \n You can't use the app without permissions", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
